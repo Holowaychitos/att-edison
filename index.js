3eb8002d2bee9d4f248f3f320ae0630681b68267
iddar <iddar@dbug.mx>
2016-01-03 12:26:46 -0600

    filter 2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const five = require('johnny-five')
 const Edison = require('galileo-io')
 const _ = require('lodash')
 
+const BEACONS = [12345, 22345, 61306, 28944, 21001]
 const transport = require('./lib/transport')
 
 const board = new five.Board({
@@ -19,8 +20,9 @@ var boardReady = () => {
     let res = _.filter([bleacons], {major: 21737})
     res = _.filter(res, {proximity: 'immediate'})
 
-    if (res.length > 0 ) {
-      console.log('minor', res[0].minor)
+    if (res.length > 0) {
+      let active = _.findIndex(BEACONS, res[0].minor)
+      console.log(active)
     }
 
     //led.off()
