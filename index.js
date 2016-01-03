32e5978237054f24c361ff6ec037265731ae67af
iddar <iddar@dbug.mx>
2016-01-03 13:00:31 -0600

    stops controller

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const Edison = require('galileo-io')
 const _ = require('lodash')
 
 const BEACONS = [12345, 22345, 61306, 28944, 21001]
+const BUS_ROUTES = {
+  12345: '1618',
+  22345: '2225',
+  61306: '2178',
+  28944: '2258',
+  21001: '2180'
+}
+
 const transport = require('./lib/transport')
 
 const board = new five.Board({
@@ -22,16 +30,18 @@ var boardReady = () => {
 
     if (res.length > 0) {
       let active = _.indexOf(BEACONS, res[0].minor)
-      console.log(active)
+      if (active !== -1) {
+        console.log(active)
+        const data = {
+          currentBusStopId: BUS_ROUTES[res[0].minor]
+        }
+
+        transport.setLevel(data)
+      }
     }
 
     //led.off()
-    // const data = {
-    //   busStopId: req.body.busStopId,
-    //   busStopLoad: req.body.busStopLoad
-    // }
 
-    // transport.setLevel(data)
   })
 }
 
